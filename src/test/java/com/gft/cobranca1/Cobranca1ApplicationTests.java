39301040286754178619ce679a815e5d8dd5e7dd
Pedrov-Santos <59704383+Pedrov-Santos@users.noreply.github.com>
2020-03-18 13:57:30 -0300

    cobrancaJAVA

---
+++ src/test/java/com/gft/cobranca1/Cobranca1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.cobranca1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cobranca1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
